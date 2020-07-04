83109d6c5394b8fdeeb058c9a0a9917b7f4b4131
Paul Colombier <45604682+laikikado@users.noreply.github.com>
2020-07-04 23:14:30 +0200

    Update ContentView.swift

--- Clicker/ContentView.swift
+++ Clicker/ContentView.swift
@@ -9,8 +9,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var score = 0
+    @State var gameIsInProgress = false
+    @State var bestScore = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                if self.score >= self.bestScore && self.bestScore != 0 {
+                    Image(systemName: "flame")
+                }
+                Text("Score : \(score)")
+            }.font(.title)
+                .padding()
+            if gameIsInProgress == true {
+                Image(systemName: "plus.square")
+                    .font(.title)
+                    .onTapGesture {
+                        self.score = self.score + 1
+                }
+            }
+            Spacer()
+            if gameIsInProgress == false {
+                Button("Nouvelle Partie") {
+                    self.score = 0
+                    self.gameIsInProgress = true
+                    Timer.scheduledTimer(withTimeInterval: 10.0, repeats: false) { (_) in
+                        self.gameIsInProgress = false
+                        if self.score > self.bestScore {
+                            self.bestScore = self.score
+                        }
+                    }
+                }.padding()
+            }
+        }
     }
 }
 
