fa11b58ad14783567c8a271c69cb3db03915698a
Paul Colombier <45604682+laikikado@users.noreply.github.com>
2020-07-04 15:18:05 +0200

    Initial Commit

---
+++ Clicker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clicker
+//
+//  Created by Paul Colombier on 04/07/2020.
+//  Copyright © 2020 Paul Colombier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
